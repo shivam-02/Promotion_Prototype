52a16305f5e23995af9d1a40c119cf1d4370ace7
shivam sugandhi <shivam.sugandhi@jda.com>
2017-01-12 15:32:20 +0530

    fixed typo in profit_forecast and add different color to progress bar

--- src/app/active-promotions/active-promotions.component.ts
+++ src/app/active-promotions/active-promotions.component.ts
@@ -12,9 +12,13 @@ export class ActivePromotionsComponent implements OnInit {
   cols:any[];
   promotions:Object[];
 
+
+
  
 
-  constructor() { }
+  constructor() {
+   
+   }
 
   ngOnInit() {
 
@@ -22,7 +26,7 @@ export class ActivePromotionsComponent implements OnInit {
 
       {field:'promotion_name',header:'Promotion'},
       {field:'duration',header:'Duration'},
-      {field:'profile_forecast',header:'Profile Forecast'},
+      {field:'profit_forecast',header:'Profit Forecast'},
       {field:'revenue_forecast',header:'Revenue Forecast' },
       {field:'unit_forecast',header:'Unit Forecast'}
 
@@ -34,29 +38,29 @@ export class ActivePromotionsComponent implements OnInit {
         {
           'promotion_name':'Promo 557',
           'duration':70,
-          'profile_forecast':93,
+          'profit_forecast':93,
           'revenue_forecast':65,
           'unit_forecast':83
         },
          {
           'promotion_name':'Promo 880',
           'duration':50,
-          'profile_forecast':55,
+          'profit_forecast':55,
           'revenue_forecast':20,
           'unit_forecast':66
         },
          {
           'promotion_name':'Promo 990',
           'duration':90,
-          'profile_forecast':80,
-          'revenue_forecast':70,
+          'profit_forecast':10,
+          'revenue_forecast':90,
           'unit_forecast':44
         },
          {
           'promotion_name':'Promo 200',
           'duration':25,
-          'profile_forecast':100,
-          'revenue_forecast':50,
+          'profit_forecast':100,
+          'revenue_forecast':10,
           'unit_forecast':70
         },
 
